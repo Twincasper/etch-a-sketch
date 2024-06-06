38b203a89c19b37c711f2fbcfe3d0a1964948ed8
Twincasper <justinwatson651@gmail.com>
2024-06-06 05:44:50 -0400

    Established default 16 x 16 grid

---
+++ script.js
@@ -0,0 +1,23 @@
+const container = document.querySelector('.container');
+const createGrid = document.querySelector('#createGrid')
+const clearGrid = document.querySelector('#clearGrid');
+const gridSize = document.querySelector('.grid-size');
+
+let rows = 16;
+let cols = 16;
+
+for (let i = 0; i < rows; i++) {
+    const row = document.createElement('div');
+    row.classList.add('row');
+    container.appendChild(row);
+
+    for (let j = 0; j < cols; j++) {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        row.appendChild(cell);
+    }
+}
+
+cell.addEventListener('mouseover', () => {
+    cell.style.backgroundColor = 'pink';
+});
